de875419e909fcca3b469943f581b51490ac7c27
Sampath Chanda <sunny.sam369@gmail.com>
2017-05-11 08:06:08 +0000

    Adding SSIM

--- Surround360-master/surround360_render/source/test/TestRenderStereoPanorama.cpp
+++ Surround360-master/surround360_render/source/test/TestRenderStereoPanorama.cpp
@@ -92,6 +92,59 @@ const Camera::Vector3 kGlobalUp = Camera::Vector3::UnitZ();
   //   printf ("a: %d\n", a);
   // }
   
+Scalar getMSSIM( const Mat& i1, const Mat& i2)
+{
+ const double C1 = 6.5025, C2 = 58.5225;
+ /***************************** INITS **********************************/
+ int d     = CV_32F;
+
+ Mat I1, I2;
+ i1.convertTo(I1, d);           // cannot calculate on one byte large values
+ i2.convertTo(I2, d);
+
+ Mat I2_2   = I2.mul(I2);        // I2^2
+ Mat I1_2   = I1.mul(I1);        // I1^2
+ Mat I1_I2  = I1.mul(I2);        // I1 * I2
+
+ /***********************PRELIMINARY COMPUTING ******************************/
+
+ Mat mu1, mu2;   //
+ GaussianBlur(I1, mu1, Size(11, 11), 1.5);
+ GaussianBlur(I2, mu2, Size(11, 11), 1.5);
+
+ Mat mu1_2   =   mu1.mul(mu1);
+ Mat mu2_2   =   mu2.mul(mu2);
+ Mat mu1_mu2 =   mu1.mul(mu2);
+
+ Mat sigma1_2, sigma2_2, sigma12;
+
+ GaussianBlur(I1_2, sigma1_2, Size(11, 11), 1.5);
+ sigma1_2 -= mu1_2;
+
+ GaussianBlur(I2_2, sigma2_2, Size(11, 11), 1.5);
+ sigma2_2 -= mu2_2;
+
+ GaussianBlur(I1_I2, sigma12, Size(11, 11), 1.5);
+ sigma12 -= mu1_mu2;
+
+ ///////////////////////////////// FORMULA ////////////////////////////////
+ Mat t1, t2, t3;
+
+ t1 = 2 * mu1_mu2 + C1;
+ t2 = 2 * sigma12 + C2;
+ t3 = t1.mul(t2);              // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+
+ t1 = mu1_2 + mu2_2 + C1;
+ t2 = sigma1_2 + sigma2_2 + C2;
+ t1 = t1.mul(t2);               // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+
+ Mat ssim_map;
+ divide(t3, t1, ssim_map);      // ssim_map =  t3./t1;
+
+ Scalar mssim = mean( ssim_map ); // mssim = average of ssim map
+ return mssim;
+}
+  
 // represents either new or old rig format depending on FLAGS_new_rig_format
 struct RigDescription {
   // new format fields
@@ -593,7 +646,7 @@ void renderStereoPanoramaChunksThread(
   *chunkR = lazyNovelChunksLR.second;
 }
 
-void generateRingOfNovelViewsAndRenderStereoSpherical (
+void st_generateRingOfNovelViewsAndRenderStereoSpherical (
     const float cameraRingRadius,
     const float camFovHorizontalDegrees,
     vector<Mat>& projectionImages,  // I/P
@@ -692,7 +745,7 @@ void generateRingOfNovelViewsAndRenderStereoSpherical (
 
 
 // generates a left/right eye equirect panorama using slices of novel views
-void st_generateRingOfNovelViewsAndRenderStereoSpherical(
+void generateRingOfNovelViewsAndRenderStereoSpherical(
     const float cameraRingRadius,
     const float camFovHorizontalDegrees,
     vector<Mat>& projectionImages,
@@ -1272,6 +1325,10 @@ void renderStereoPanorama() {
   cv::bitwise_xor(sphericalImageR_cn1, st_sphericalImageR_cn1, diff2);
   bool eq1 = (cv::countNonZero(diff1) == 0);
   bool eq2 = (cv::countNonZero(diff2) == 0);
+
+  cv::Scalar ssimL = getMSSIM(sphericalImageL, st_sphericalImageL);
+  cv::Scalar ssimR = getMSSIM(sphericalImageR, st_sphericalImageR);
+  LOG(INFO) << "OPT correctness: ssim: L" << ssimL << " R: " << ssimR ;
 
   if (eq1 && eq2) {
     LOG(INFO) << "Finished SingleThreaded Run" ;
